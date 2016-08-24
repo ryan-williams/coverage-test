2b20aa70c5aa22b1a2ead05fec02dde5478603bd
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-24 02:38:56 +0000

    SomaticAllele:7

---
+++ src/main/scala/org/hammerlab/guacamole/reference/SomaticAllele.scala
@@ -0,0 +1,5 @@
+package org.hammerlab.guacamole.reference
+
+case class SomaticAllele(somaticLogOdds: Double) {
+  lazy val foo = somaticLogOdds
+}
--- src/main/scala/org/hammerlab/guacamole/variants/CalledSomaticAllele.scala
+++
@@ -1,5 +0,0 @@
-package org.hammerlab.guacamole.variants
-
-case class CalledSomaticAllele(somaticLogOdds: Double) {
-  lazy val foo = somaticLogOdds
-}
