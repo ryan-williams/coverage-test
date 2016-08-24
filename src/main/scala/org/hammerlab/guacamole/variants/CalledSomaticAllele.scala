eeee18937de9ec69d1d92fa0a45ab2cb62c23cbd
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-24 02:02:36 +0000

    still CalledSomaticAllele:54

--- src/main/scala/org/hammerlab/guacamole/variants/CalledSomaticAllele.scala
+++ src/main/scala/org/hammerlab/guacamole/variants/CalledSomaticAllele.scala
@@ -1,30 +1,8 @@
 package org.hammerlab.guacamole.variants
 
 import org.bdgenomics.adam.util.PhredUtils
-import org.hammerlab.guacamole.reference.{ContigName, Locus, NumLoci}
 
-/**
- *
- * A variant that exists in a tumor sample, but not in the normal sample; includes supporting read statistics from both samples
- *
- * @param sampleName sample the variant was called on
- * @param contigName chromosome or genome contig of the variant
- * @param start start position of the variant (0-based)
- * @param allele reference and sequence bases of this variant
- * @param somaticLogOdds log odds-ratio of the variant in the tumor compared to the normal sample
- * @param rsID   identifier for the variant if it is in dbSNP
- * @param length length of the variant
- */
-case class CalledSomaticAllele(sampleName: String,
-                               contigName: ContigName,
-                               start: Locus,
-                               allele: Allele,
-                               somaticLogOdds: Double,
-                               rsID: Option[Int] = None,
-                               override val length: NumLoci = 1) extends ReferenceVariant {
-  val end: Locus = start + 1L
-
-  // P ( variant in tumor AND no variant in normal) = P(variant in tumor) * P(reference in normal)
+case class CalledSomaticAllele(somaticLogOdds: Double) {
   lazy val phredScaledSomaticLikelihood =
     PhredUtils.successProbabilityToPhred(somaticLogOdds)
 }
