ab777e3299e24c8a96e7759f93f33c1b75ad4758
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-24 02:30:46 +0000

    CalledSomaticAllele:9

--- src/main/scala/org/hammerlab/guacamole/reference/Interval.scala
+++ src/main/scala/org/hammerlab/guacamole/reference/Interval.scala
@@ -1,19 +1,9 @@
 package org.hammerlab.guacamole.reference
 
-trait Interval extends Ordered[Interval] {
+trait Interval {
   def start: Long
 
   def end: Long
-
-  def contains(locus: Long): Boolean = start <= locus && locus < end
-
-  /** Comparisons between ranges. Order is DESCENDING (i.e. reversed) from by start. */
-  override def compare(other: Interval): Int = {
-    val diff = start - other.start
-    if (diff < 0) -1
-    else if (diff == 0) 0
-    else 1
-  }
 }
 
 private case class IntervalImpl(start: Long, end: Long)
--- src/main/scala/org/hammerlab/guacamole/variants/CalledSomaticAllele.scala
+++ src/main/scala/org/hammerlab/guacamole/variants/CalledSomaticAllele.scala
@@ -1,5 +1,5 @@
 package org.hammerlab.guacamole.variants
 
 case class CalledSomaticAllele(somaticLogOdds: Double) {
-  lazy val phredScaledSomaticLikelihood = somaticLogOdds
+  lazy val foo = somaticLogOdds
 }
